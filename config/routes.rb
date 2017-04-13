9006d9c6562d21113d6c9c61513f79ca7e6f00bf
yamaneko <yunisy1018@gmail.com>
2017-04-13 05:09:26 +0000

    bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+
+
 resources :pictures,only:[:index,:new,:create,:edit,:update,:destroy]do
   collection do
     post :confirm
   end
  end
+ 
+ root'top#index'
   get 'pictures' => 'pictures#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
